5068694d75ac8b9fca6b742466ccf250ddae4904
RothlisbergerDaniel <dlrothlis@gmail.com>
2024-03-14 17:03:42 -0400

    Task 4b complete

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -1,11 +1,15 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
 {
+    public TextMeshProUGUI selectedText;
     public static Villager SelectedVillager { get; private set; }
+
+
     public static void SetSelectedVillager(Villager villager)
     {
         if(SelectedVillager != null)
@@ -15,5 +19,19 @@ public class CharacterControl : MonoBehaviour
         SelectedVillager = villager;
         SelectedVillager.Selected(true);
     }
-    
+
+    private void Update()
+    {
+        
+        if (SelectedVillager == null)
+        {
+            selectedText.text = ("Selected: None");
+        }
+        else
+        {
+            selectedText.text = ("Selected: " + SelectedVillager.CanOpen());
+        }
+    }
+
+
 }
